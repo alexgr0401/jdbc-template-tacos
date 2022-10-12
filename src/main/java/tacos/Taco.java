ad60abd0bd2e00ff13d1cc05b65f7f40c151f979
alexgr0401 <109770902+alexgr0401@users.noreply.github.com>
2022-10-12 21:30:43 +0200

    update Ingredient

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,4 +1,5 @@
 package tacos;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -11,19 +12,19 @@ import lombok.Data;
 @Data
 public class Taco {
 
-  private Long id;
+	private Long id;
 
-  private Date createdAt = new Date();
+	private Date createdAt = new Date();
 
-  @NotNull
-  @Size(min=5, message="Name must be at least 5 characters long")
-  private String name;
+	@NotNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
+	private String name;
 
-  @Size(min=1, message="You must choose at least 1 ingredient")
-  private List<IngredientRef> ingredients = new ArrayList<>();
+	@Size(min = 1, message = "You must choose at least 1 ingredient")
+	private List<IngredientRef> ingredients = new ArrayList<>();
 
-  public void addIngredient(Ingredient taco) {
-    this.ingredients.add(new IngredientRef(taco.getId()));
-  }
+	public void addIngredient(Ingredient taco) {
+		this.ingredients.add(new IngredientRef(taco.getId()));
 
+	}
 }
